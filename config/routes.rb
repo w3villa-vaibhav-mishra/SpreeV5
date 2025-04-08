54abd3782898e41a466ee6414ba8fdde54f15bc3
Vaibhav Mishra <vaibhav.mishra@w3villa.com>
2025-04-08 16:41:39 +0530

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+require "sidekiq/web" # require the web UI
+
+Rails.application.routes.draw do
+  # This line mounts Spree's routes at the root of your application.
+  # This means, any requests to URLs such as /products, will go to
+  # Spree::ProductsController.
+  # If you would like to change where this engine is mounted, simply change the
+  # :at option to something different.
+  #
+  # We ask that you don't use the :as option here, as Spree relies on it being
+  # the default of "spree".
+  mount Spree::Core::Engine, at: '/'
+  devise_for :users, class_name: "Spree::User"
+
+  mount Sidekiq::Web => "/sidekiq" # access it at http://localhost:3000/sidekiq
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  root "spree/home#index"
+end
